7050393541a6a72c91b1dcef410dc6bc93d51cd1
星祭 <xingchenmengjing@163.com>
2020-02-14 22:36:12 +0800

    逃出飞船V1

--- ex43.py
+++ ex43.py
@@ -37,13 +37,21 @@ class Engine(object):
             x.enter()
 class Death(Scene):
     
+    quips = [
+        "You died. You kinda suck at this.",
+        "Your mom would be proud...if she were smarter.",
+        "Such a luser.",
+        "I have a small puppy that's better at this.",
+        "You're worse than your Dad's jokes."
+    ]
     def enter(self):
-        print (" You are dead! ")
+        print (Death.quips[randint(0,len(self.quips)-1)])
+        exit(1)
 
 class CentralCorridor(Scene):
 
     def enter(self):
-        pass
+        print ()
 
 class LaserWeponArmory(Scene):
     
