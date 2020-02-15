9642bdd209a258002182d8842050875b4e51f13f
星祭 <xingchenmengjing@163.com>
2020-02-15 15:38:12 +0800

    逃出飞船V

--- ex43.py
+++ ex43.py
@@ -2,15 +2,15 @@ from sys import exit
 from random import randint
 from textwrap import dedent
 class Scene(object):
-
+#场景 类
     def enter(self):
-        print("This scene is not yet config ured.")
-        print ("Subclass it and implement enter().")
+        print("This scene is not yet configured.") #场景尚未配置
+        print ("Subclass it and implement enter().") #分类并实现enter（）
         exit(1)
 
 
 class Engine(object):
-
+#引擎 类
     def __init__(self,scene_map):
        self.scene_map = scene_map
 
@@ -22,19 +22,19 @@ class Engine(object):
             current_scene = self.scene_map.next_scene(next_scene_name)
             #be sure to print out the last scene
             current_scene.enter()
-    def yes_no (self):
-        while True:
-            put = input("yes or no >> ")
-            if put == 'no':
-                exit()
-            elif put == 'yes':
-                break
-            else:
-                print("plase agent ") 
-    def judge_dead(self,life):
-        if life == 0:
-            x = Death()
-            x.enter()
+    # def yes_no (self):
+    #     while True:
+    #         put = input("yes or no >> ")
+    #         if put == 'no':
+    #             exit()
+    #         elif put == 'yes':
+    #             break
+    #         else:
+    #             print("plase agent ") 
+    # def judge_dead(self,life):
+    #     if life == 0:
+    #         x = Death()
+    #         x.enter()
 class Death(Scene):
     
     quips = [
@@ -51,42 +51,146 @@ class Death(Scene):
 class CentralCorridor(Scene):
 
     def enter(self):
-        print ()
+        print (dedent(""""
+                here we go 
+                shoot 
+                dodge
+                tell a joke
+                """))
+        action = input("> ")
+
+        if action == "shoot!":
+            print (dedent(""""
+                    Then he eats you.
+                    """))
+            return 'death'
+        elif action == "dodge!":
+            print (dedent(""""
+                     Gothon stomps on your head and eats you.
+                    """))
+            return 'death'
+        elif action == "tell a joke":
+            print (dedent(""""
+                        Lucky for you 
+                        """))
+            return 'laser_weapon_armory'
+
+        else:
+            print ("DOES NOT COMPUTE!")
+            return 'central_corridor'
 
 class LaserWeponArmory(Scene):
     
     def enter(self):
-        pass
+        print (dedent("""
+                武器库 开锁 十次三位数密码
+                """))
+        
+        #code = f"{randint(1,9)}{randint(1,9)}{randint(1,9)}"
+        code = 233
+        guess_1 = input("[keypad]> ")
+        guess = int(guess_1)
+        guesses = 0
+        while guess != code and guesses < 10:
+            print ("BZZZZEDDD!")
+            if guess > code:
+                print ("so big")
+            elif guess < code:
+                print ("so small")
+            guesses +=1
+            guess_1 = input("[keypad]> ")
+            guess = int(guess_1)
+
+        if guess == code:
+            print (dedent("""
+                    成功 继续
+                    """))
+            return 'the_bridge'
+        else:
+            print (dedent("""
+                    错误~~
+                    """))
+            return 'death'
+
 
 class TheBridge(Scene):
 
     def enter(self):
-        pass
+        print (dedent("""
+                炸桥~
+                扔炸弹？ throw the bomb
+                慢慢放？ slowly place the bomb 
+                """))
+        action = input("> ")
+        if action == "throw the bomb":
+            print (dedent("""
+                    you die
+                    """))
+            return 'death'
+        
+        elif action == "slowly place the bomb":
+            print (dedent("""
+                    lucky ~
+                    """))
+            return 'escape_pod'
+        else:
+            print ("DOES NOT COMPUTE!")
+            return "the_bridge"
 
 class EscapePod(Scene):
 
     def enter(self):
-        pass
+        print (dedent("""
+                五个逃生舱 选一个
 
+                """))
+        
+        good_pod = 5
+        guess = input("[pod #]> ")
+
+        if int(guess) != good_pod:
+            print (dedent("""
+                    你选择了 {guess} you die
+                    """))
+            return 'death'
+        else :
+            print ((dedent("""
+                   你选择了 {guess} lucky~
+                    """)))
+            return 'finished'
+
+class Finished(Scene):
+
+    def enter(self):
+        print ("You won! Good job.")
+        return 'finished'
 
 class Map(object):
 
+    scenes = {
+        'central_corridor' : CentralCorridor(),
+        'laser_weapon_armory' : LaserWeponArmory(),
+        'the_bridge' : TheBridge(),
+        'escape_pod' : EscapePod(),
+        'death' : Death(),
+        'finished' : Finished(),
+    }
     def __init__(self,start_scene):
-        print (f"Now,open map {start_scene}")
         self.start_scene = start_scene
 
     def next_scene(self,scene_name):
-        pass
+        val = Map.scenes.get(scene_name)
+        return val
 
     def opening_scene(self):
-        pass
+        return self.next_scene(self.start_scene)
 
 
 
 a_map = Map('central_corridor')
 a_game = Engine(a_map)
-a_game.yes_no()
+
 a_game.play()
-a_game.judge_dead(0)
-test = globals()
-print (test)
+
+# test = globals()
+# print (test)
