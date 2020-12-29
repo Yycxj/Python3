a1da3385a5735f5423d851c128e0eb663055a4c5
local <yzyasr@163.com>
2020-12-29 20:55:21 +0800

    6

--- ex6.py
+++ ex6.py
@@ -5,16 +5,17 @@ binary = "binary"
 do_not = "don't"
 y = f"Those who know {binary} and those who {do_not}."
 
-print (x)
-print (y)
+# print (x)
+# print (y)
 
-print (f"I said: {x}")
-print (f"I also said:'{y}'")
+# print (f"I said: {x}")
+# print (f"I also said:'{y}'")
 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! {}"
 
 print (joke_evaluation.format(hilarious))
+# print ("yes,",joke_evaluation.format{hilarious})
 
 w = "This is the left side of ..."
 e = "a string with a right side."
