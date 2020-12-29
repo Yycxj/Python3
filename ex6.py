05055e1a8f118fee16dccb7df888643019f6053e
local <yzyasr@163.com>
2020-12-29 21:08:46 +0800

    format 的应用

--- ex6.py
+++ ex6.py
@@ -15,7 +15,7 @@ hilarious = False
 joke_evaluation = "Isn't that joke so funny?! {}"
 
 print (joke_evaluation.format(hilarious))
-# print ("yes,",joke_evaluation.format{hilarious})
+# print (f"yes,{joke_evaluation.format{hilarious}}"")   #
 
 w = "This is the left side of ..."
 e = "a string with a right side."
