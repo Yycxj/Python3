fe0e69c6940989cd2b04ebd77a24281fa5ac1c55
local <yzyasr@163.com>
2020-12-30 21:59:25 +0800

    open

--- ex15.py
+++ ex15.py
@@ -13,3 +13,13 @@ file_again = input('>> ')
 txt_again = open (file_again)
 
 print (txt_again.read())
+
+f = open ('test.py','r+')
+data = f.read()
+print (data)
+f.write ('write1')
+print (data)
+f.close()
+f = open ('test.py','r+')
+data = f.read()
+print (data)
