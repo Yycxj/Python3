157284301e7ef5484f00c8cedcf01635fed1b832
local <yzyasr@163.com>
2021-01-05 21:09:57 +0800

    ～

--- ex20.py
+++ ex20.py
@@ -4,14 +4,17 @@ script,input_file = argv
 
 def print_all (f):
     print (f.read())
+    #输出全部
     
 def rewind(f):
     f.seek(0,0)
+    #把文件读写位置回档
     
 def print_a_line (line_count,f):
     print (line_count , f.readline())
-    
+    #单行输出
 current_file = open (input_file)
+#打开文件
 
 print ("First let's print the whole file:\n")
 
