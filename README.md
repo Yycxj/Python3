e188702513dda15e6bb3e16c5e8265856e72af99
Tengxun <yzyasr@163.com>
2021-12-15 22:15:41 +0800

    ·

--- README.md
+++
@@ -1,7 +0,0 @@
-# Python3
-Python3 code 
-《笨方法学Python3》代码合集
-
-ex6 - ex9 print 
-
-ex11 -ex14 argv、input
--- ex23-1.py
+++
@@ -1,8 +0,0 @@
-
-def test (a):
-    line = a.readline()
-    if line :
-        print (f"##{line}")
-        test (a)
-txt = open ('annex/languages.txt')
-test (txt)
